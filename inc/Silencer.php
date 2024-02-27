58854eca3a15763b43d836436e01fdc6c425558d
AHaldner <andrin.haldner@stutz-medien.ch>
2024-02-27 16:44:51 +0100

    feat: add new hiding options

--- inc/Silencer.php
+++ inc/Silencer.php
@@ -27,11 +27,20 @@ class Silencer {
 		add_filter( 'comments_open', [ $this, 'disable_comments_status' ], 20, 2 );
 		add_filter( 'pings_open', [ $this, 'disable_comments_status' ], 20, 2 );
 		add_filter( 'comments_array', [ $this, 'disable_comments_hide_existing_comments' ], 10, 2 );
-		add_action( 'admin_menu', [ $this, 'disable_comments_admin_menu' ] );
-		add_action( 'admin_init', [ $this, 'disable_comments_admin_menu_redirect' ] );
-		add_action( 'admin_init', [ $this, 'disable_comments_dashboard' ] );
 		add_action( 'wp_before_admin_bar_render', [ $this, 'disable_comments_admin_bar' ] );
 		add_action( 'init', [ $this, 'disable_comments_on_media_attachments' ] );
+
+		$hide_settings = get_option( 'hide_settings' );
+
+		if ( '1' === $hide_settings ) {
+			add_action( 'admin_menu', [ $this, 'disable_comments_admin_menu' ] );
+			add_action( 'admin_init', [ $this, 'disable_comments_admin_menu_redirect' ] );
+			add_action( 'wp_dashboard_setup', [ $this, 'disable_comments_dashboard' ] );
+			add_action( 'admin_head', [ $this, 'hide_comments_from_activity_widget' ] );
+		}
+
+		add_action( 'admin_menu', [ $this, 'create_settings_page' ] );
+		add_action( 'admin_init', [ $this, 'register_settings' ] );
 	}
 
 	/**
@@ -97,7 +106,7 @@ class Silencer {
 	 * @return void
 	 */
 	public function disable_comments_dashboard() {
-		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' ); // Recent Comments
 	}
 
 	/**
@@ -133,5 +142,71 @@ class Silencer {
 			10,
 			2
 		);
+	}
+
+
+	public function hide_comments_from_activity_widget() {
+		echo '
+		<style>
+		.comment-count, #latest-comments {
+			display: none;
+		}
+
+		#published-posts {
+			border-bottom: none;
+		}
+		</style>
+		';
+	}
+
+	public function create_settings_page() {
+		add_submenu_page(
+			'options-general.php',
+			'Silencer Options',
+			'Silencer',
+			'manage_options',
+			'silencer-options',
+			array( $this, 'silencer_options_page' )
+		);
+	}
+
+	public function register_settings() {
+		register_setting(
+			'silencer-settings-group',
+			'hide_settings',
+			array(
+				'type'              => 'string',
+				'sanitize_callback' => 'sanitize_text_field',
+				'default'           => 'none',
+			)
+		);
+	}
+
+	public function silencer_options_page() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		echo '<div class="wrap">';
+		echo '<h1>' . esc_html( get_admin_page_title() ) . '</h1>';
+		echo '<form method="post" action="options.php">';
+
+		settings_fields( 'silencer-settings-group' );
+
+		do_settings_sections( 'silencer-options' );
+
+		$hide_settings = get_option( 'hide_settings' );
+
+		echo '<table class="form-table">';
+		echo '<tr valign="top">';
+		echo '<th scope="row">Hide Comment Options</th>';
+		echo '<td><input type="checkbox" id="hide_settings" name="hide_settings" value="1" ' . checked( 1, $hide_settings, false ) . ' /></td>';
+		echo '</tr>';
+		echo '</table>';
+
+		submit_button( 'Save Settings' );
+
+		echo '</form>';
+		echo '</div>';
 	}
 }
